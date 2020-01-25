48c23ed9205ff7e3420936a55d0dfcb4e615c186
James Stamps Jr <jstampshere@gmail.com>
2020-01-25 17:25:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,9 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if(age) = 13 >= 19
+  return = "You are a teenager!"
+  else = "You are not a teenager"
 }
 
 function ageChecker(age) {
